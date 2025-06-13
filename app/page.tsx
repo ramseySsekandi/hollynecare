7b3d665906bd01795bd4dbca458d0a930fd502bf
ssekandiRamsey <moshehravens@gmail.com>
2025-06-13 11:12:43 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg">Welcome to the Next.js app with Tailwind CSS!</p>
+      <p className="text-lg">This is a simple example of a Next.js app using Shadcn components with dark mode.</p>
+      <Button>
+        Heading to GitHub
+      </Button>
+      <ModeToggle />
+      <p>Testing deployment</p>
+    </main>
+  );
+}
